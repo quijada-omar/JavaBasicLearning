7adf515d2cf78c154e034f28f6ced9be519e0a6f
Jesus Omar <omar.quijada4@hotmail.com>
2023-03-23 22:38:24 -0700

    Update CheckedExceptionsDemo.java

--- JavaPrograming/src/day14/CheckedExceptionsDemo.java
+++ JavaPrograming/src/day14/CheckedExceptionsDemo.java
@@ -2,4 +2,12 @@ package day14;
 
 public class CheckedExceptionsDemo {
 
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Program started");
+		System.out.println("Program is in progress");
+		Thread.sleep(5000);
+		System.out.println("Program is finished");
+		System.out.println("Program is terminated");
+		
+	}
 }
